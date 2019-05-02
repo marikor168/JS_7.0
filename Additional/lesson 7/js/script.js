3150879481fa0fef51bf6706415b5b566d265c77
Mariya <marikor168@gmail.com>
2019-05-02 13:04:00 +0300

    Lesson 7 Additional Corrected

--- Additional/lesson 7/js/script.js
+++ Additional/lesson 7/js/script.js
@@ -3,14 +3,18 @@ let rocket = document.querySelector('.rocket'),
     posX = 0,
     posY = 0;
 
+let myReq;
+
 function myAnimation() {
     posX +=2;
     posY -=2;
     rocket.style.top = posY + 'px';
     rocket.style.left = posX + 'px';
-    requestAnimationFrame(myAnimation);
+    if (posX <= 805) {
+        myReq = requestAnimationFrame(myAnimation);
+    }
 }
 
-
-
 btn.addEventListener('click', myAnimation);
+cancelAnimationFrame(myReq);
+
